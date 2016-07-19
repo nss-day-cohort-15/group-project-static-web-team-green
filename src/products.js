130f7c2560bea2a6597267f403886586ecfa1158
Delaine Wendling <delaine.wendling@gmail.com>
2016-07-18 20:21:43 -0500

    Converted the objects in the array into individual arrays

--- src/products.js
+++ src/products.js
@@ -1,87 +1,120 @@
 var products = [
   {
     name: "Reusable Bag",
-    description: "Jean shorts portland letterpress, tousled roof party gluten-free single-origin coffee. Next level kinfolk mixtape put a bird on it salvia dreamcatcher. Literally meditation godard food truck, keffiyeh schlitz intelligentsia shoreditch tousled artisan. Heirloom 3 wolf moon bushwick literally kinfolk, tilde pork belly gastropub pop-up austin gluten-free. Deep v skateboard heirloom, fap yuccie wolf cronut listicle narwhal. Umami narwhal fixie knausgaard next level before they sold out vegan. Meh health goth humblebrag, cliche hoodie 90's listicle brooklyn trust fund."
+    description: "Jean shorts portland letterpress, tousled roof party gluten-free single-origin coffee. Next level kinfolk mixtape put a bird on it salvia dreamcatcher. Literally meditation godard food truck, keffiyeh schlitz intelligentsia shoreditch tousled artisan. Heirloom 3 wolf moon bushwick literally kinfolk, tilde pork belly gastropub pop-up austin gluten-free. Deep v skateboard heirloom, fap yuccie wolf cronut listicle narwhal. Umami narwhal fixie knausgaard next level before they sold out vegan. Meh health goth humblebrag, cliche hoodie 90's listicle brooklyn trust fund.",
     price: "$6.78",
     image: "images/reusableBag.jpg"
-  }
+  },
   {
     name: "Eco-Friendly Soap",
-    description: "Jean shorts portland letterpress, tousled roof party gluten-free single-origin coffee. Next level kinfolk mixtape put a bird on it salvia dreamcatcher. Literally meditation godard food truck, keffiyeh schlitz intelligentsia shoreditch tousled artisan. Heirloom 3 wolf moon bushwick literally kinfolk, tilde pork belly gastropub pop-up austin gluten-free. Deep v skateboard heirloom, fap yuccie wolf cronut listicle narwhal. Umami narwhal fixie knausgaard next level before they sold out vegan. Meh health goth humblebrag, cliche hoodie 90's listicle brooklyn trust fund."
+    description: "Jean shorts portland letterpress, tousled roof party gluten-free single-origin coffee. Next level kinfolk mixtape put a bird on it salvia dreamcatcher. Literally meditation godard food truck, keffiyeh schlitz intelligentsia shoreditch tousled artisan. Heirloom 3 wolf moon bushwick literally kinfolk, tilde pork belly gastropub pop-up austin gluten-free. Deep v skateboard heirloom, fap yuccie wolf cronut listicle narwhal. Umami narwhal fixie knausgaard next level before they sold out vegan. Meh health goth humblebrag, cliche hoodie 90's listicle brooklyn trust fund.",
     price: "$4.21/bar",
     image: "images/soap.jpg"
-  }
+  },
   {
     name: "Eco-Friendly Detergent",
-    description: "Jean shorts portland letterpress, tousled roof party gluten-free single-origin coffee. Next level kinfolk mixtape put a bird on it salvia dreamcatcher. Literally meditation godard food truck, keffiyeh schlitz intelligentsia shoreditch tousled artisan. Heirloom 3 wolf moon bushwick literally kinfolk, tilde pork belly gastropub pop-up austin gluten-free. Deep v skateboard heirloom, fap yuccie wolf cronut listicle narwhal. Umami narwhal fixie knausgaard next level before they sold out vegan. Meh health goth humblebrag, cliche hoodie 90's listicle brooklyn trust fund."
+    description: "Jean shorts portland letterpress, tousled roof party gluten-free single-origin coffee. Next level kinfolk mixtape put a bird on it salvia dreamcatcher. Literally meditation godard food truck, keffiyeh schlitz intelligentsia shoreditch tousled artisan. Heirloom 3 wolf moon bushwick literally kinfolk, tilde pork belly gastropub pop-up austin gluten-free. Deep v skateboard heirloom, fap yuccie wolf cronut listicle narwhal. Umami narwhal fixie knausgaard next level before they sold out vegan. Meh health goth humblebrag, cliche hoodie 90's listicle brooklyn trust fund.",
     price: "$5.88",
     image: "images/detergent.jpg"
-  }
+  },
   {
     name: "Eco-Friendly Trash Bags",
-    description: "Jean shorts portland letterpress, tousled roof party gluten-free single-origin coffee. Next level kinfolk mixtape put a bird on it salvia dreamcatcher. Literally meditation godard food truck, keffiyeh schlitz intelligentsia shoreditch tousled artisan. Heirloom 3 wolf moon bushwick literally kinfolk, tilde pork belly gastropub pop-up austin gluten-free. Deep v skateboard heirloom, fap yuccie wolf cronut listicle narwhal. Umami narwhal fixie knausgaard next level before they sold out vegan. Meh health goth humblebrag, cliche hoodie 90's listicle brooklyn trust fund."
+    description: "Jean shorts portland letterpress, tousled roof party gluten-free single-origin coffee. Next level kinfolk mixtape put a bird on it salvia dreamcatcher. Literally meditation godard food truck, keffiyeh schlitz intelligentsia shoreditch tousled artisan. Heirloom 3 wolf moon bushwick literally kinfolk, tilde pork belly gastropub pop-up austin gluten-free. Deep v skateboard heirloom, fap yuccie wolf cronut listicle narwhal. Umami narwhal fixie knausgaard next level before they sold out vegan. Meh health goth humblebrag, cliche hoodie 90's listicle brooklyn trust fund.",
     price: "$5.21/box",
     image: "images/trashBag.jpg"
-  }
+  },
   {
     name: "Tree-free Journal",
-    description: "Jean shorts portland letterpress, tousled roof party gluten-free single-origin coffee. Next level kinfolk mixtape put a bird on it salvia dreamcatcher. Literally meditation godard food truck, keffiyeh schlitz intelligentsia shoreditch tousled artisan. Heirloom 3 wolf moon bushwick literally kinfolk, tilde pork belly gastropub pop-up austin gluten-free. Deep v skateboard heirloom, fap yuccie wolf cronut listicle narwhal. Umami narwhal fixie knausgaard next level before they sold out vegan. Meh health goth humblebrag, cliche hoodie 90's listicle brooklyn trust fund."
+    description: "Jean shorts portland letterpress, tousled roof party gluten-free single-origin coffee. Next level kinfolk mixtape put a bird on it salvia dreamcatcher. Literally meditation godard food truck, keffiyeh schlitz intelligentsia shoreditch tousled artisan. Heirloom 3 wolf moon bushwick literally kinfolk, tilde pork belly gastropub pop-up austin gluten-free. Deep v skateboard heirloom, fap yuccie wolf cronut listicle narwhal. Umami narwhal fixie knausgaard next level before they sold out vegan. Meh health goth humblebrag, cliche hoodie 90's listicle brooklyn trust fund.",
     price: "$11.76",
     image: "images/journal.jpg"
-  }
+  },
   {
     name: "LED Bulbs",
-    description: "Jean shorts portland letterpress, tousled roof party gluten-free single-origin coffee. Next level kinfolk mixtape put a bird on it salvia dreamcatcher. Literally meditation godard food truck, keffiyeh schlitz intelligentsia shoreditch tousled artisan. Heirloom 3 wolf moon bushwick literally kinfolk, tilde pork belly gastropub pop-up austin gluten-free. Deep v skateboard heirloom, fap yuccie wolf cronut listicle narwhal. Umami narwhal fixie knausgaard next level before they sold out vegan. Meh health goth humblebrag, cliche hoodie 90's listicle brooklyn trust fund."
+    description: "Jean shorts portland letterpress, tousled roof party gluten-free single-origin coffee. Next level kinfolk mixtape put a bird on it salvia dreamcatcher. Literally meditation godard food truck, keffiyeh schlitz intelligentsia shoreditch tousled artisan. Heirloom 3 wolf moon bushwick literally kinfolk, tilde pork belly gastropub pop-up austin gluten-free. Deep v skateboard heirloom, fap yuccie wolf cronut listicle narwhal. Umami narwhal fixie knausgaard next level before they sold out vegan. Meh health goth humblebrag, cliche hoodie 90's listicle brooklyn trust fund.",
     price: "$3.22/bulb",
     image: "images/ledLight.jpg"
-  }
+  },
   {
     name: "Solar Powered Phone Charger (rugged)",
-    description: "Jean shorts portland letterpress, tousled roof party gluten-free single-origin coffee. Next level kinfolk mixtape put a bird on it salvia dreamcatcher. Literally meditation godard food truck, keffiyeh schlitz intelligentsia shoreditch tousled artisan. Heirloom 3 wolf moon bushwick literally kinfolk, tilde pork belly gastropub pop-up austin gluten-free. Deep v skateboard heirloom, fap yuccie wolf cronut listicle narwhal. Umami narwhal fixie knausgaard next level before they sold out vegan. Meh health goth humblebrag, cliche hoodie 90's listicle brooklyn trust fund."
+    description: "Jean shorts portland letterpress, tousled roof party gluten-free single-origin coffee. Next level kinfolk mixtape put a bird on it salvia dreamcatcher. Literally meditation godard food truck, keffiyeh schlitz intelligentsia shoreditch tousled artisan. Heirloom 3 wolf moon bushwick literally kinfolk, tilde pork belly gastropub pop-up austin gluten-free. Deep v skateboard heirloom, fap yuccie wolf cronut listicle narwhal. Umami narwhal fixie knausgaard next level before they sold out vegan. Meh health goth humblebrag, cliche hoodie 90's listicle brooklyn trust fund.",
     price: "$59.81",
     image: "images/solarCharger.jpg"
-  }
+  },
   {
     name: "Compostable Cutlery",
-    description: "Jean shorts portland letterpress, tousled roof party gluten-free single-origin coffee. Next level kinfolk mixtape put a bird on it salvia dreamcatcher. Literally meditation godard food truck, keffiyeh schlitz intelligentsia shoreditch tousled artisan. Heirloom 3 wolf moon bushwick literally kinfolk, tilde pork belly gastropub pop-up austin gluten-free. Deep v skateboard heirloom, fap yuccie wolf cronut listicle narwhal. Umami narwhal fixie knausgaard next level before they sold out vegan. Meh health goth humblebrag, cliche hoodie 90's listicle brooklyn trust fund."
+    description: "Jean shorts portland letterpress, tousled roof party gluten-free single-origin coffee. Next level kinfolk mixtape put a bird on it salvia dreamcatcher. Literally meditation godard food truck, keffiyeh schlitz intelligentsia shoreditch tousled artisan. Heirloom 3 wolf moon bushwick literally kinfolk, tilde pork belly gastropub pop-up austin gluten-free. Deep v skateboard heirloom, fap yuccie wolf cronut listicle narwhal. Umami narwhal fixie knausgaard next level before they sold out vegan. Meh health goth humblebrag, cliche hoodie 90's listicle brooklyn trust fund.",
     price: "$3.15/pack",
     image: "images/cutlery.jpg"
-  }
+  },
   {
     name: "Compost Turner",
-    description: "Jean shorts portland letterpress, tousled roof party gluten-free single-origin coffee. Next level kinfolk mixtape put a bird on it salvia dreamcatcher. Literally meditation godard food truck, keffiyeh schlitz intelligentsia shoreditch tousled artisan. Heirloom 3 wolf moon bushwick literally kinfolk, tilde pork belly gastropub pop-up austin gluten-free. Deep v skateboard heirloom, fap yuccie wolf cronut listicle narwhal. Umami narwhal fixie knausgaard next level before they sold out vegan. Meh health goth humblebrag, cliche hoodie 90's listicle brooklyn trust fund."
+    description: "Jean shorts portland letterpress, tousled roof party gluten-free single-origin coffee. Next level kinfolk mixtape put a bird on it salvia dreamcatcher. Literally meditation godard food truck, keffiyeh schlitz intelligentsia shoreditch tousled artisan. Heirloom 3 wolf moon bushwick literally kinfolk, tilde pork belly gastropub pop-up austin gluten-free. Deep v skateboard heirloom, fap yuccie wolf cronut listicle narwhal. Umami narwhal fixie knausgaard next level before they sold out vegan. Meh health goth humblebrag, cliche hoodie 90's listicle brooklyn trust fund.",
     price: "$91.50",
     image: "images/composter.jpg"
-  }
+  },
   {
     name: "Bamboo Toothbrush",
-    description: "Jean shorts portland letterpress, tousled roof party gluten-free single-origin coffee. Next level kinfolk mixtape put a bird on it salvia dreamcatcher. Literally meditation godard food truck, keffiyeh schlitz intelligentsia shoreditch tousled artisan. Heirloom 3 wolf moon bushwick literally kinfolk, tilde pork belly gastropub pop-up austin gluten-free. Deep v skateboard heirloom, fap yuccie wolf cronut listicle narwhal. Umami narwhal fixie knausgaard next level before they sold out vegan. Meh health goth humblebrag, cliche hoodie 90's listicle brooklyn trust fund."
+    description: "Jean shorts portland letterpress, tousled roof party gluten-free single-origin coffee. Next level kinfolk mixtape put a bird on it salvia dreamcatcher. Literally meditation godard food truck, keffiyeh schlitz intelligentsia shoreditch tousled artisan. Heirloom 3 wolf moon bushwick literally kinfolk, tilde pork belly gastropub pop-up austin gluten-free. Deep v skateboard heirloom, fap yuccie wolf cronut listicle narwhal. Umami narwhal fixie knausgaard next level before they sold out vegan. Meh health goth humblebrag, cliche hoodie 90's listicle brooklyn trust fund.",
     price: "$3.70",
     image: "images/toothbrush.jpg"
-  }
+  },
   {
     name: "Stainless Steel Water Bottle",
-    description: "Jean shorts portland letterpress, tousled roof party gluten-free single-origin coffee. Next level kinfolk mixtape put a bird on it salvia dreamcatcher. Literally meditation godard food truck, keffiyeh schlitz intelligentsia shoreditch tousled artisan. Heirloom 3 wolf moon bushwick literally kinfolk, tilde pork belly gastropub pop-up austin gluten-free. Deep v skateboard heirloom, fap yuccie wolf cronut listicle narwhal. Umami narwhal fixie knausgaard next level before they sold out vegan. Meh health goth humblebrag, cliche hoodie 90's listicle brooklyn trust fund."
+    description: "Jean shorts portland letterpress, tousled roof party gluten-free single-origin coffee. Next level kinfolk mixtape put a bird on it salvia dreamcatcher. Literally meditation godard food truck, keffiyeh schlitz intelligentsia shoreditch tousled artisan. Heirloom 3 wolf moon bushwick literally kinfolk, tilde pork belly gastropub pop-up austin gluten-free. Deep v skateboard heirloom, fap yuccie wolf cronut listicle narwhal. Umami narwhal fixie knausgaard next level before they sold out vegan. Meh health goth humblebrag, cliche hoodie 90's listicle brooklyn trust fund.",
     price: "$12.45",
     image: "images/waterBottle.jpg"
-  }
+  },
   {
     name: "Reduce your Carbon Footprint Cookbook",
-    description: "Jean shorts portland letterpress, tousled roof party gluten-free single-origin coffee. Next level kinfolk mixtape put a bird on it salvia dreamcatcher. Literally meditation godard food truck, keffiyeh schlitz intelligentsia shoreditch tousled artisan. Heirloom 3 wolf moon bushwick literally kinfolk, tilde pork belly gastropub pop-up austin gluten-free. Deep v skateboard heirloom, fap yuccie wolf cronut listicle narwhal. Umami narwhal fixie knausgaard next level before they sold out vegan. Meh health goth humblebrag, cliche hoodie 90's listicle brooklyn trust fund."
+    description: "Jean shorts portland letterpress, tousled roof party gluten-free single-origin coffee. Next level kinfolk mixtape put a bird on it salvia dreamcatcher. Literally meditation godard food truck, keffiyeh schlitz intelligentsia shoreditch tousled artisan. Heirloom 3 wolf moon bushwick literally kinfolk, tilde pork belly gastropub pop-up austin gluten-free. Deep v skateboard heirloom, fap yuccie wolf cronut listicle narwhal. Umami narwhal fixie knausgaard next level before they sold out vegan. Meh health goth humblebrag, cliche hoodie 90's listicle brooklyn trust fund.",
     price: "$29.99",
     image: "images/cookbook.jpg"
-  }
+  },
   {
     name: "'Make your own Eco-friendly Products' Recipe Book",
-    description: "Jean shorts portland letterpress, tousled roof party gluten-free single-origin coffee. Next level kinfolk mixtape put a bird on it salvia dreamcatcher. Literally meditation godard food truck, keffiyeh schlitz intelligentsia shoreditch tousled artisan. Heirloom 3 wolf moon bushwick literally kinfolk, tilde pork belly gastropub pop-up austin gluten-free. Deep v skateboard heirloom, fap yuccie wolf cronut listicle narwhal. Umami narwhal fixie knausgaard next level before they sold out vegan. Meh health goth humblebrag, cliche hoodie 90's listicle brooklyn trust fund."
+    description: "Jean shorts portland letterpress, tousled roof party gluten-free single-origin coffee. Next level kinfolk mixtape put a bird on it salvia dreamcatcher. Literally meditation godard food truck, keffiyeh schlitz intelligentsia shoreditch tousled artisan. Heirloom 3 wolf moon bushwick literally kinfolk, tilde pork belly gastropub pop-up austin gluten-free. Deep v skateboard heirloom, fap yuccie wolf cronut listicle narwhal. Umami narwhal fixie knausgaard next level before they sold out vegan. Meh health goth humblebrag, cliche hoodie 90's listicle brooklyn trust fund.",
     price: "$27.86",
     image: "images/alternatives.jpg"
   }
 ]
 
-for (key in products) {
-  document.getElementById("productContainer").innerHTML += key;
+console.log(products[0]);
+// for (i=0; i<products.length; i++) {
+//   for (key in products) {
+//     document.getElementById("productContainer").innerHTML += key;
+//   }
+// }
+
+function getFields(input, field) {
+    var output = [];
+    for (var i=0; i < input.length ; ++i)
+        output.push(input[i][field]);
+    return output;
 }
+
+var names = getFields(products,"name");
+var descriptions = getFields(products, "description");
+var prices = getFields(products, "price");
+var images = getFields(products, "image");
+var container = document.getElementById("productContainer");
+
+// function buildItem (key, tag) {
+//   for (i=0; i<key.length; i++) {
+//     container.innerHTML += "<" + tag+ ">" + key[i] + "</" +tag+ ">";
+//   }
+// }
+
+// buildItem (names, "h2");
+
+// for (i=0; i<names.length; i++) {
+//   // console.log(names[i]);
+//   document.getElementById("productContainer").innerHTML += "<div>"
+
+//   "<h2>" + names[i] + "</h2>";
+// }
+
+console.log(names);
 
 
 
