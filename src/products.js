1652f48dc48e656daa114af2dfd24e8863f764df
Delaine Wendling <delaine.wendling@gmail.com>
2016-07-19 13:00:15 -0500

    Made clickable images

--- src/products.js
+++ src/products.js
@@ -115,8 +115,28 @@ if (container) {
   for (i=0; i<names.length; i++) {
   container.innerHTML += "<div class='productCard'> <h2 class='productName'>" + names[i] + "</h2> <img src = '" + images[i]+ "'" + " class='productImage'> <p class='productDescription'>" + descriptions[i] + "</p> <p class='productPrice'> <strong> Price: </strong> " + prices[i] + "</p> </div>";
   }
+  // for (i=0; i<divArray.length; i++) {
+  //   divArray[i].className = "productCard";
+  // }
+  var imageArray = document.getElementsByClassName("productImage");
+  for (i=0; i<imageArray.length; i++) {
+      imageArray[i].addEventListener('click', function(event){
+        console.log(event)
+        var parentDiv = event.target.parentElement;
+        var image = event.target;
+        var nextElement = event.target.nextElementSibling;
+        var prevElement = event.target.previousElementSibling;
+        parentDiv.classList.toggle('fullDiv');
+        parentDiv.classList.toggle('productCard');
+        image.classList.toggle("clickImage");
+        image.classList.toggle("productImage");
+        prevElement.classList.toggle('hidden');
+        prevElement.classList.toggle('productName');
+        nextElement.classList.toggle('hidden');
+        nextElement.classList.toggle('productDescription');
+    });
+  }
 }
-
 
 //  Loop for featured products
 if (featuredContainer) {
@@ -125,4 +145,6 @@ if (featuredContainer) {
     featuredContainer.innerHTML += "<div class='featuredCard'> <h2 class='featuredName'>" + names[i] + "</h2> <img src = '" + images[i]+ "'" + " class='featuredImage'> <p class='featuredDescription'>" + descriptions[i] + "</p> <p class='featuredPrice'>" + prices[i] + "</p> </div>";
   }
 }
+
+
 
