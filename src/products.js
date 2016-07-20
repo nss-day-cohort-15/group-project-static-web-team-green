d838b19a598a1739ca733012cc8769d98e2467fc
Dayne Wright <daynewr@gmail.com>
2016-07-20 14:44:47 -0500

    added SPA js logic

--- src/products.js
+++ src/products.js
@@ -125,3 +125,33 @@ if (featuredContainer) {
     featuredContainer.innerHTML += "<div class='productCard'> <h2 class='procustName'>" + names[i] + "</h2> <img src = '" + images[i]+ "'" + " class='productImage'> <p class='productDescription'>" + descriptions[i] + "</p> <p class='productPrice'>" + prices[i] + "</p> </div>";
   }
 }
+
+
+// SPA Modifications //
+
+console.log('Welcome to our website fellow developer!')
+
+var navLinks = document.querySelectorAll('li')
+var pages = document.querySelectorAll('.page')
+
+showPage(location.hash)
+
+for (var i = 0; i < navLinks.length; i++) {
+  navLinks[i].addEventListener('click', clickedNavLink);
+}
+
+function clickedNavLink (evt) {
+  for (var i = 0; i < pages.length; i++) {
+    pages[i].classList.add('hidden')
+  }
+
+  showPage(evt.target.hash)
+}
+
+function showPage (id) {
+  if (id) {
+    document.querySelector(id).classList.remove('hidden')
+  } else {
+    document.querySelector('#home').classList.remove('hidden')
+  }
+}
